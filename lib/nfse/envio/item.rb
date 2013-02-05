e2a147c22e891c825696de11341488c7e5086339
Carlos Eduardo L. Lopes <carlos.el.lopes@gmail.com>
2013-02-05 13:13:44 -0200

    Refactored Nfse::Envio::Item class

--- lib/nfse/envio/item.rb
+++ lib/nfse/envio/item.rb
@@ -1,30 +1,12 @@
 module Nfse
   module Envio
     class Item < Nfse::Base
-      attr_accessor :discriminacao, :tributavel
-      attr_reader :quantidade, :valor_unitario
+      include Virtus
 
-      def initialize(attributes = {})
-        @quantidade = 0.0
-        @valor_unitario = 0.0
-        attributes.each do |k,v|
-          send("#{k}=", v)
-        end
-      end
-
-      # Atribui um valor para a quantidade
-      # rescue NoMethodError para quando for passado um object que não responde a #to_f
-      def quantidade=(value)
-        @quantidade = value.to_f
-      rescue NoMethodError
-      end
-
-      # Atribui um valor para o valor_unitario
-      # rescue NoMethodError para quando for passado um object que não responde a #to_f
-      def valor_unitario=(value)
-        @valor_unitario = value.to_f
-      rescue NoMethodError
-      end
+      attribute :discriminacao, String
+      attribute :quantidade, Float, default: 0
+      attribute :valor_unitario, Float, default: 0
+      attribute :tributavel, String
 
       def valor_total
         @quantidade * @valor_unitario
--- test/lib/nfse/envio/item_test.rb
+++ test/lib/nfse/envio/item_test.rb
@@ -12,40 +12,34 @@ describe Nfse::Envio::Item do
   end
 
   describe 'quantidade attribute' do
-    it 'must be 0.0 if no value was passed' do
-      subject.quantidade.must_equal 0.0
+    it 'must have the accessors methods' do
+      subject.must_respond_to :quantidade
+      subject.must_respond_to :quantidade=
     end
 
-    it "must keep the old value if is passed a object that doesn't respond to #to_f" do
-      subject.quantidade = 2.5
-      subject.quantidade = false
-
-      subject.quantidade.must_equal 2.5
+    it 'must have the right default value' do
+      subject.quantidade.must_equal 0
     end
 
-    it 'must convert correctly if is passed a valid float value' do
-      subject.quantidade = '13.67'
-
-      subject.quantidade.must_equal 13.67
+    it 'must coerce the value to float' do
+      subject.quantidade = '1.2'
+      subject.quantidade.must_equal 1.2
     end
   end
 
   describe 'valor_unitario attribute' do
-    it 'must be 0.0 if no value was passed' do
-      subject.valor_unitario.must_equal 0.0
+    it 'must have the accessors methods' do
+      subject.must_respond_to :valor_unitario
+      subject.must_respond_to :valor_unitario=
     end
 
-    it "must keep the old value if is passed a object that doesn't respond to #to_f" do
-      subject.valor_unitario = 2.5
-      subject.valor_unitario = false
-
-      subject.valor_unitario.must_equal 2.5
+    it 'must have the right default value' do
+      subject.valor_unitario.must_equal 0
     end
 
-    it 'must convert correctly if is passed a valid float value' do
-      subject.valor_unitario = '13.67'
-
-      subject.valor_unitario.must_equal 13.67
+    it 'must coerce the value to float' do
+      subject.valor_unitario = '1.2'
+      subject.valor_unitario.must_equal 1.2
     end
   end
 
@@ -69,29 +63,6 @@ describe Nfse::Envio::Item do
     end
   end
 
-  describe 'initialize passing a hash of attributes' do
-    before do
-      @attr = {
-        discriminacao:  'Item exemplo',
-        quantidade:     '10',
-        valor_unitario: '1.00',
-        tributavel:     'S'
-      }
-
-      @item = Nfse::Envio::Item.new(@attr)
-
-      # Formata os valores para eles baterem corretamente com o retorno dos getters
-      @attr[:quantidade]     = @attr[:quantidade].to_f
-      @attr[:valor_unitario] = @attr[:valor_unitario].to_f
-    end
-
-    it 'must have the right attributes values' do
-      @attr.each do |k,v|
-        @item.send(k).must_equal v
-      end
-    end
-  end
-
   describe '#render' do
     describe 'campinas' do
       before do
@@ -100,8 +71,8 @@ describe Nfse::Envio::Item do
 
       it 'must render the right xml' do
         subject.discriminacao  = 'Item 1'
-        subject.quantidade     = '2.0'
-        subject.valor_unitario = '3.0'
+        subject.quantidade     = 2.0
+        subject.valor_unitario = 3.0
         subject.tributavel     = 'S'
 
         xml('Item', str: subject.render).must_equal xml('Item[1]', prefeitura: :campinas, file: :envio)
